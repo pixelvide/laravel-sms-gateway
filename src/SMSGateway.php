4c7cd0adb4aafd8a1a8046e4585e1481945b7260
Nitin Goyal <nitingoyal.dev@gmail.com>
2022-06-28 10:25:17 +0530

    Send Bulk SMS Feature added

--- src/SMSGateway.php
+++ src/SMSGateway.php
@@ -43,21 +43,30 @@ class SMSGateway
     /**
      * @param  SMS  $sms
      * @return array
+     */
+    private function buildSMSPayload (SMS $sms): array
+    {
+        return [
+            'templateId' => $sms->getTemplateId(),
+            'substitutes' => $sms->getSubstitutes(),
+            'recipients' => $sms->getRecipients(),
+        ];
+    }
+
+    /**
+     * @param  array  $payload
+     * @return array
      *
      * @throws VariableMissingException
      */
-    private function gatewayPayload(SMS $sms): array
+    private function gatewayPayload(array $payload): array
     {
         if ($this->isNullOrEmptyString($this->smsGatewayEndpoint)) {
             throw new VariableMissingException('SMS_GATEWAY_ENDPOINT variable is either unspecified or empty');
         }
         return [
             'FunctionName' => $this->smsGatewayEndpoint,
-            'Payload'      => json_encode([
-                'templateId'  => $sms->getTemplateId(),
-                'substitutes' => $sms->getSubstitutes(),
-                'recipients'  => $sms->getRecipients(),
-            ]),
+            'Payload' => json_encode($payload),
         ];
     }
 
@@ -69,7 +78,28 @@ class SMSGateway
     public function send(SMS $sms)
     {
         $sms->validate();
-        $response = $this->lambdaClient->invoke($this->gatewayPayload($sms));
+
+        $response = $this->lambdaClient->invoke($this->gatewayPayload($this->buildSMSPayload($sms)));
+        $result = $response->get('Payload')
+            ->getContents();
+
+        return json_decode($result, true);
+    }
+
+    /**
+     * @param  array  $smsArr
+     * @return mixed
+     * @throws VariableMissingException
+     */
+    public function sendBulk(array $smsArr) {
+        $gwPayload = array();
+        foreach ($smsArr as $sms) {
+            $sms->validate();
+
+            $gwPayload[] = $this->buildSMSPayload($sms);
+        }
+
+        $response = $this->lambdaClient->invoke($this->gatewayPayload($gwPayload));
         $result = $response->get('Payload')
             ->getContents();
 
